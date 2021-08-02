1b3fc6405e4adf653147c9ce43b6c2d1711e3123
Brittney Jackson <brnjackson@yahoo.com>
2021-08-02 02:00:04 -0400

    tried different methods to pass failing test - test is now passing but all other tests broken

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const validateLineup = (lineup) => {
-  const salaries = lineup.reduce((totalSum, lineup) => {
-    return lineup.salary + totalSum
-  }, 0)
-
   for (let i = 0; i < lineup.length; i++) {
     let count = lineup[i]
+    const salaries = lineup.reduce((totalSum, lineup) => {
+      return lineup.salary + totalSum
+    }, 0)
+    let validSalary = true
+    let validPosition = true
+    let validTeam = true
+    let validGame = true
+    let completeLineup = true
 
-    if (count.position['OF'] <= 3 && count.position['P', 'C', '1B', '2B', 'SS'] === 1) {
-      return true
+    if (count.position['OF'] > 3 && count.position['P', 'C', '1B', '2B', 'SS'] !== 1) {
+      validPosition = false
     }
-    if (count.teamid >= 2 || count.gameid >= 3) {
-      return true
-    }
-    if (salaries <= 45000) {
-      return true }
+    if (count.teamid < 2) {
+      validTeam = false }
 
-    else { return false }
+    if (count.gameid < 3) {
+      validGame = false
+    }
+
+    if (salaries > 45000) {
+      validSalary = true }
+
+    if (validGame === true && validSalary === true && validTeam === true &&
+        validPosition === true) { completeLineup = true }
+
+    return completeLineup
   }
 }
 
